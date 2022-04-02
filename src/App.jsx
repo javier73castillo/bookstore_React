4662e50409f0e3c630d50c10bbf7fc6171836c08
cristian <cristianfarriol19@hotmail.com>
2022-04-02 15:31:00 +0200

    books component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-import './main.scss';
-import { BrowserRouter as Router, NavLink, Route, Routes } from "react-router-dom";
+import "./main.scss";
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { LoginPage } from "./pages/LoginPage/LoginPage";
 import { RegisterPage } from "./pages/RegisterPage/RegisterPage";
-import { RequireAuth } from './shared/components/RequireAuth/RequireAuth';
-import { JwtContext } from './shared/contexts/JwtContext';
-import { useState } from "react" 
-import { ButtonLogOut } from "./shared/components/ButtonLogOut/ButtonLogOut"
-import { ShoppingCart } from './components/ShoppingCart/ShoppingCart';
+import { RequireAuth } from "./shared/components/RequireAuth/RequireAuth";
+import { JwtContext } from "./shared/contexts/JwtContext";
+import { useState } from "react";
+import { ButtonLogOut } from "./shared/components/ButtonLogOut/ButtonLogOut";
+import { ShoppingCart } from "./components/ShoppingCart/ShoppingCart";
+import { Books } from "./components/Books/Books";
 
 function App() {
   const [jwt, setJwt] = useState(localStorage.getItem("token"));
 
   return (
     <JwtContext.Provider value={{ jwt, setJwt }}>
-    <div className="App">
-      <Router>
-      <nav className="nav">
+      <div className="App">
+        <Router>
+          <nav className="nav">
             {jwt && <NavLink to="/">Home</NavLink>}
             {!jwt && (
               <>
@@ -24,17 +30,19 @@ function App() {
                 <NavLink to="/register">Register</NavLink>
               </>
             )}
-            {jwt && <ButtonLogOut/>}
+            {jwt && <ButtonLogOut />}
             <ShoppingCart />
-            </nav>
+          </nav>
           <Routes>
-           {/*  <Route path="/" element={<RequireAuth><HomePage/></RequireAuth>}/> */}
-           <Route path="/" element={<HomePage/>}/>
-            <Route path="/login" element={<LoginPage/>}/>
-            <Route path="/register" element={<RegisterPage/>}/>
-          </Routes>     
-      </Router>  
-    </div>
+            {/*  <Route path="/" element={<RequireAuth><HomePage/></RequireAuth>}/> */}
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+          </Routes>
+        </Router>
+
+        <Books />
+      </div>
     </JwtContext.Provider>
   );
 }
--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -1,40 +1,44 @@
-import axios from 'axios';
-import React from 'react'
-import { useEffect, useState } from 'react'
+import axios from "axios";
+import React from "react";
+import { useEffect, useState } from "react";
+import "./_Books.scss";
 
+export const Books = () => {
+  const [books, setBooks] = useState([]);
 
+  const urlPage = `https://library-api-rest-86hi8hunh-javier73castillo.vercel.app/api/books`;
 
- export const Books = () => {
-    const [books, setBooks] = useState([]);
-
-    const urlPage = `https://library-api-rest-86hi8hunh-javier73castillo.vercel.app/api/books`
-  
-
-   useEffect(() => {
-     axios
-     .get(urlPage)
-     .then((response)=>{
+  useEffect(() => {
+    axios.get(urlPage).then((response) => {
       console.log(response);
-     setBooks(response.data) 
-     });
-   }, [urlPage]);
-
+      setBooks(response.data);
+    });
+  }, [urlPage]);
 
   return (
-    <>   
-        <div className='cards'>
-              {books.map((book)=>{
-                return(
-                  <div>
-                    <h2>{book.name}</h2>
-                    <h3>{book.editorial}</h3>
-                    <h3>{book.year}</h3>
-                    <img src={book.img} alt={book.name}/>
-                  </div>
+    <>
+      <div className="cards container">
+        {books.map((book) => {
+          return (
+            <div className="cards__books">
+              <img
+                className="cards__books--img"
+                src={book.img}
+                alt={book.name}
+              />
+              <div className="cards__books--text">
+                <h2>{book.name}</h2>
+                <h3>{book.editorial}</h3>
+                <h3>{book.year}</h3>
+              </div>
 
-                );
-              })}
+              <button className="cards__books--button">
+                Agregar libro al carrito
+              </button>
             </div>
+          );
+        })}
+      </div>
     </>
-  )
-}
+  );
+};
