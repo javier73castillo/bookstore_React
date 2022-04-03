a290192f468f3d783c7a8a72ecf84c28214b4c89
cristian <cristianfarriol19@hotmail.com>
2022-04-03 23:37:39 +0200

    carrito

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import { useBookContext } from '../../shared/contexts/BookContext'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useBookContext } from "../../shared/contexts/BookContext";
 
 export const Cart = () => {
-    const {items} = useBookContext();
-    console.log(items[0])
+  const { items } = useBookContext();
+  console.log(items);
+  let total = 0;
   return (
-    <div className=''>
-        {items.map(libro => {
-            return (
-                <div className='bookRow'>
-                    <p>{libro.name}</p>
-                    <p>{libro.price}</p>
-                </div>
-            )
-        })}
+    <div className="">
+      {items.map((libro) => {
+        return (
+          <div className="bookRow">
+            <p>{libro.name}</p>
+            <p>€{libro.price}</p>
+          </div>
+        );
+      })}
+      {(items.reduce((prev, current) => console.log(prev + current.price)), 0)}
+      <Link to="/">
+        <button>Volver al inicio</button>
+      </Link>
     </div>
-  )
-}
+  );
+};
