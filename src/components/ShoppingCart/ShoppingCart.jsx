fc3890d451b88453aa4138542d3550b651af44eb
cristian <cristianfarriol19@hotmail.com>
2022-04-04 21:52:59 +0200

    act

--- src/components/ShoppingCart/ShoppingCart.jsx
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import { BsCart4 } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useBookContext } from "../../shared/contexts/BookContext";
 
 export const ShoppingCart = () => {
+  const { items } = useBookContext();
+
+  let counter = items.reduce((acc, current) => acc + current.count, 0);
+
   return (
     <>
-    <Link to="/cart">
-      <button className="carrito">
-        <BsCart4 />
-      </button>
+      <Link to="/cart">
+        <button className="carrito">
+          <BsCart4 />
+          <span className="price">{counter}</span>
+        </button>
       </Link>
     </>
-    
   );
 };
