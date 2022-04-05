b03c83fd3a33c189bd644e4266888bfec5afdbbb
cristian <cristianfarriol19@hotmail.com>
2022-04-05 13:00:27 +0200

    warning loginform

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { API } from "../../shared/services/api";
 import { useNavigate } from "react-router-dom";
@@ -6,6 +6,7 @@ import { JwtContext } from "../../shared/contexts/JwtContext";
 import { useContext } from "react";
 
 export const LoginForm = () => {
+  const [value, setValue] = useState(true);
   let navigate = useNavigate();
   const { setJwt } = useContext(JwtContext);
   const { register, handleSubmit } = useForm();
@@ -18,6 +19,12 @@ export const LoginForm = () => {
       localStorage.setItem("token", response.data);
       navigate("/");
     });
+  };
+
+  const controlForm = (e) => {
+    e.preventDefault(e);
+    e.target.form[0].value === "" && setValue(false);
+    e.target.form[1].value === "" && setValue(false);
   };
 
   return (
@@ -41,7 +48,13 @@ export const LoginForm = () => {
             /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-]).{8,12}$/,
         })}
       />
-      <button className="botonSubmit">Login</button>
+      <button className="botonSubmit" onClick={controlForm}>
+        Login
+      </button>
+
+      {value === false && (
+        <h1 className="warning">Todos los campos son obligatorios</h1>
+      )}
     </form>
   );
 };
